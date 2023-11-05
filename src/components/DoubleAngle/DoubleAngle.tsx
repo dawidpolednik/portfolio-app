5be568ae97075a789de76676f4c9b2da1dd885bb
Dawid Polednik <dawidpolednik@dawid.local>
2023-11-05 10:38:01 +0100

    investigate portal

--- src/components/DoubleAngle/DoubleAngle.tsx
+++ src/components/DoubleAngle/DoubleAngle.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
-import { Link } from 'react-scroll';
+// import { Link } from 'react-scroll';
 import styles from './DoubleAngle.module.scss';
-import AngleUpIcon from '~/images/angle-up.svg';
+import AngleUpIcon from '~/images/angle-up-icon.svg';
+import AngleDownIcon from '~/images/angle-down-icon.svg';
+import Link from 'next/link';
 
 interface DoubleAngleProps {
   onUp?: boolean;
@@ -9,21 +11,22 @@ interface DoubleAngleProps {
 }
 
 export const DoubleAngle: FC<DoubleAngleProps> = ({ onUp, subPage }) => (
-  <Link
-    activeClass='active'
-    className={styles.doubleAngle}
-    to={subPage}
-    spy={true}
-    smooth={true}
-    hashSpy={true}
-    offset={0}
-    duration={500}
-    delay={250}
-    isDynamic={true}
-    ignoreCancelEvents={false}
-  >
+  // <Link
+  //   activeClass='active'
+  //   className={styles.doubleAngle}
+  //   to={subPage}
+  //   spy={true}
+  //   smooth={true}
+  //   hashSpy={true}
+  //   offset={0}
+  //   duration={500}
+  //   delay={250}
+  //   isDynamic={true}
+  //   ignoreCancelEvents={false}
+  // >
+  <Link href={`#${subPage}`} className={styles.doubleAngle}>
     {!onUp ? (
-      <i className='fa fa-angle-double-down ' aria-hidden='true'></i>
+      <AngleDownIcon className={'angle-down'} />
     ) : (
       <AngleUpIcon className={'angle-up'} />
     )}
